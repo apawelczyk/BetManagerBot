bc5f4b7109f4de12301fcd3a328e201c0e283587
apawelczyk <46108090+apawelczyk@users.noreply.github.com>
2021-01-09 12:35:37 -0600

    Add files via upload

---
+++ BetManager.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+# Get Bot token from settings file
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
+token = os.getenv("TOKEN")
+
+# Connect Bot to Discord
+client = discord.Client()
+
+# On Bot Start
+@client.event
+async def on_ready():
+    print("Started!")
+
+# On Discord Message
+@client.event
+async def on_message(message):
+    # Block Bot from sending because of itself
+    if message.author == client.user:
+        return
+
+    await message.channel.send("Test 3!")
+    return
+
+# Launch Bot
+client.run(token)
